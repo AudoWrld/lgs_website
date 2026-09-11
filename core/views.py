ebe7fdf53df64ff38f51e972c50720e1c48c430c
Victor Owino <vo4685336@gmail.com>
2026-09-11 05:51:59 +0300

    Update view functions to use correct template paths for improved organization

--- core/views.py
+++ core/views.py
@@ -2,24 +2,24 @@ from django.shortcuts import render
 
 
 def homepage(request):
-    return render(request, "homepage.html")
+    return render(request, "core/homepage.html")
 
 
 def services(request):
-    return render(request, "services.html")
+    return render(request, "core/services.html")
 
 
 def about(request):
-    return render(request, "about.html")
+    return render(request, "core/about.html")
 
 
 def pricing(request):
-    return render(request, "pricing.html")
+    return render(request, "core/pricing.html")
 
 
 def contact(request):
-    return render(request, "contact.html")
+    return render(request, "core/contact.html")
 
 
 def request_quote(request):
-    return render(request, "request_quote.html")
+    return render(request, "core/request_quote.html")
