a641e7ea727eaccd6dc928d71edf38d7411b8e17
Victor Owino <vo4685336@gmail.com>
2026-09-17 21:06:59 +0300

    Refactor access control in reception views: replace login_required with custom reception_required decorator

--- reception/views.py
+++ reception/views.py
@@ -1,47 +1,61 @@
+from functools import wraps
+
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
 
 
-@login_required(login_url="accounts:login")
+def reception_required(view):
+    @wraps(view)
+    @login_required(login_url="accounts:login")
+    def wrapped(request, *args, **kwargs):
+        if not request.user.is_reception:
+            raise PermissionDenied("Reception access is required.")
+        return view(request, *args, **kwargs)
+
+    return wrapped
+
+
+@reception_required
 def reception_dashboard(request):
     return render(request, "reception/reception_dashboard.html")
 
 
-@login_required(login_url="accounts:login")
+@reception_required
 def client_search(request):
     return render(request, "reception/client_search.html")
 
 
-@login_required(login_url="accounts:login")
+@reception_required
 def client_register(request):
     return render(request, "reception/client_register.html")
 
 
-@login_required(login_url="accounts:login")
+@reception_required
 def sample_registration(request):
     return render(request, "reception/sample_registration.html")
 
 
-@login_required(login_url="accounts:login")
+@reception_required
 def coa_reporting_preference(request):
     return render(request, "reception/coa_reporting_preference.html")
 
 
-@login_required(login_url="accounts:login")
+@reception_required
 def generate_worksheet(request):
     return render(request, "reception/generate_worksheet.html")
 
 
-@login_required(login_url="accounts:login")
+@reception_required
 def client_submission_form(request):
     return render(request, "reception/client_submission_form.html")
 
 
-@login_required(login_url="accounts:login")
+@reception_required
 def payment_details(request):
     return render(request, "reception/payment_details.html")
 
 
-@login_required(login_url="accounts:login")
+@reception_required
 def add_expense(request):
     return render(request, "reception/add_expense.html")
