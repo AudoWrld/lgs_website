b9169f773dd48262a698d1ddff48c54222fd56cb
Victor Owino <vo4685336@gmail.com>
2026-09-17 06:29:34 +0300

    Add URL routing for reception: include reception app URLs in main urlpatterns

--- lgs_website/urls.py
+++ lgs_website/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("core.urls")),
     path("accounts/", include("accounts.urls")),
+    path("reception/", include("reception.urls")),
 ]
