dc55ad782c1cbef55125d8728e39b5df0a59ff8f
Victor Owino <vo4685336@gmail.com>
2026-09-25 21:25:45 +0300

    Add nav_active template tag for dynamic navigation highlighting

---
+++ chemist/templatetags/chemist_extras.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def nav_active(context, *url_names):
+    request = context.get("request")
+    if not request or not request.resolver_match:
+        return ""
+    return "active" if request.resolver_match.url_name in url_names else ""
