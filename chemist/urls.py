aac9ea90ec39fdb2cf18144e15e0c36ed0e37aa8
Victor Owino <vo4685336@gmail.com>
2026-09-25 21:38:08 +0300

    Add views and URL patterns for mineral analysis, metallurgical analysis, reassay samples, and QC approved pages

--- chemist/urls.py
+++ chemist/urls.py
@@ -5,4 +5,12 @@ app_name = "chemist"
 
 urlpatterns = [
     path("dashbord/", views.chemist_dashboard, name="chemist_dashboard"),
+    path("mineral_analysis/", views.mineral_analysis, name="mineral_analysis"),
+    path(
+        "metallurgical_analysis/",
+        views.metallurgical_analysis,
+        name="metallurgical_analysis",
+    ),
+    path("reassay_samples/", views.reassay_samples, name="reassay_samples"),
+    path("qc_approved/", views.qc_approved, name="qc_approved"),
 ]
--- chemist/views.py
+++ chemist/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
 
+
 def chemist_dashboard(request):
     return render(request, "chemist/chemist_dashboard.html")
+
+
+def mineral_analysis(request):
+    return render(request, "chemist/mineral_analysis.html")
+
+
+def metallurgical_analysis(request):
+    return render(request, "chemist/metallurgical_analysis.html")
+
+
+def reassay_samples(request):
+    return render(request, "chemist/reassay_samples.html")
+
+
+def qc_approved(request):
+    return render(request, "chemist/qc_approved.html")
